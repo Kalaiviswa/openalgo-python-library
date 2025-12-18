b9d4d2b082ecea7a7ce092132a6ecbe5752f891b
kalaivani <panlani@gmail.com>
2025-12-18 13:59:30 +0530

    Version change from 1.0.43 to 1.0.44

--- openalgo/__init__.py
+++ openalgo/__init__.py
@@ -87,7 +87,7 @@ class api(OrderAPI, DataAPI, AccountAPI, FeedAPI, OptionsAPI, TelegramAPI, Utili
         self.quotes_callback = None
         self.depth_callback = None
 
-__version__ = "1.0.43"
+__version__ = "1.0.44"
 
 # Export main components for easy access
 __all__ = ['api', 'Strategy', 'ta', 'nbjit', 'prange']
